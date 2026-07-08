eeb8776164dd0ca8773b4928e4d4eecdbdf054b4
3elinda <belinda.cedillo_santiago24@student.cart.org>
2026-07-07 18:31:27 -0700

    Build Categories page and pass selection to Game page
    - Build Categories page with four category buttons
    - Use map() to generate buttons dynamically from a list
    - Pass selected category to Game page using React Router state
    - Game page now displays the anme of the chosen category

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,7 +1,35 @@
+import { useNavigate } from "react-router-dom";
+
+const categories = [
+  { name: "Movies" },
+  { name: "Animals" },
+  { name: "Countries" },
+  { name: "Random" },
+];
+// const categories is a list of category objects
+
 function Categories() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Categories Page</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-8">
+      <h1 className="text-4xl font-bold">Choose a Category</h1>
+      <div className="flex flex-col gap-4">
+        {categories.map((category) => (
+          // .map() loops through categories list and creates a button for each one automatically instead of writing 5 buttons by hand
+          <button
+            key={category.name}
+            // "React needs a unique key on each item it created in a loop so it can keep track of them>" Always add when using .map()
+            className="px-8 py-4 tet-xl border-4 border-black rounded-full hover:bg-balck hover:text-white transition all"
+            onClick={
+              () => navigate("/game", { state: { category: category.name } })
+              // passings state between pages. "when navigating to /game a small piece of info is handed, the category name clicked. The game page will read the "note" and know which category to use"
+            }
+          >
+            {category.name}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router-dom";
+
 function Game() {
+  // useLocation is a React Router tool that lets info to be read about current page, including any state passed to it
+  // location.state?.category read the category name from that "note" passed. The ? is a safety check
+  const location = useLocation();
+  const category = location.state?.category;
+
   return (
-    <div>
-      <h1>Game Page</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-8">
+      <h1 className="text-4xl font-bold">{category}</h1>
+      <p>Game goes here</p>
     </div>
   );
 }
